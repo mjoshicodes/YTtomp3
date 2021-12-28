a787097afa648f399e5b67d1a361e680e475b4c8
mjoshicodes <megha.joshi@yale.edu>
2021-12-27 18:18:54 -0800

    Basic loading of search

--- YTtomp3.js
+++ YTtomp3.js
@@ -1,1 +1,15 @@
+const puppeteer = require('puppeteer');
 
+(async () => {
+const browser = await puppeteer.launch({headless: false});
+const page = await browser.newPage();
+await page.goto('https://320ytmp3.com/en31/');
+await page.$eval('#go > div > input.form-control.form-control-lg', el => el.value = 'https://www.youtube.com/watch?v=kJQP7kiw5Fk');
+await page.click("#go > div > button");
+await page.waitForNavigation();
+await page.click("#cvt-btn");
+
+//await browser.close();
+})();
+// document.querySelector("#go > div > button")
+// document.querySelector("#cvt-btn")
